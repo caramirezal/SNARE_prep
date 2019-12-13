3817e8927d40d0395d698da25dcbe874a253bb15
caramirezal <caramirezal@ciencias.unam.mx>
2019-12-13 17:55:17 +0100

    commands to download data from gseo

---
+++ data/download.sh
@@ -0,0 +1,16 @@
+## Dowloading files
+## fastq1
+#wget \
+#  --tries=5 \
+#  https://sra-pub-src-1.s3.amazonaws.com/SRR9672090/SC_20190509A_AdMsBrain_SNARE_chromatin_S16_R1_001.fastq.gz.1 \
+#  -O data/SC_20190509A_AdMsBrain_SNARE_chromatin_S16_R1_001.fastq.gz
+## fastq2
+wget \
+  --tries=5 \
+  https://sra-pub-src-1.s3.amazonaws.com/SRR9672090/SC_20190509A_AdMsBrain_SNARE_chromatin_S16_R2_001.fastq.gz.1 \
+  -O data/SC_20190509A_AdMsBrain_SNARE_chromatin_S16_R2_001.fastq.gz
+## fastq3
+wget \
+  --tries=5 \
+  https://sra-pub-src-1.s3.amazonaws.com/SRR9672090/SC_20190509A_AdMsBrain_SNARE_chromatin_S16_R3_001.fastq.gz.1 \
+  -O data/SC_20190509A_AdMsBrain_SNARE_chromatin_S16_R3_001.fastq.gz
